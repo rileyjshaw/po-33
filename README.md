de08feef9d6ddb96e69d343edfa4073cd89e28f4
Riley Shaw <rileyjshaw@gmail.com>
2020-07-29 03:00:31 +0000

    Transfer from Glitch

---
+++ README.md
@@ -0,0 +1,75 @@
+# PO-33
+
+A drum sample loader for my [Pocket Operator K.O.](https://teenage.engineering/guides/po-33/en)
+
+# [~\*~ TRY IT HERE ~*~](http://po-33.glitch.me/)
+
+## Usage
+
+Drag and drop an audio file onto a pad. Click to play the sample. Once you've loaded a few, plug in your K.O. and click “Play all” to record the samples into a drum bank.
+
+### Adding multiple samples at the same time
+
+You can drag in multiple files at the same time. The first sample is assigned to whichever pad you dragged to. The rest of the files fill subsequent pads. Anything after Pad 16 gets cut off.
+
+### Listening to samples
+
+By default, samples will play once when you load them. To listen to them again, click the corresponding pad.
+
+If you want to try your setup before loading it to the K.O., the following keyboard shortcuts map to pads:
+
+|   |   |   |   |
+|---|---|---|---|
+| 1 | 2 | 3 | 4 |
+| Q | W | E | R |
+| A | S | D | F |
+| Z | X | C | V |
+|   |   |   |   |
+
+### Reordering / deleting samples
+
+You can reorder samples by dragging them. If you drag a sample out of the container, it's deleted.
+
+### Settings
+
+Click the nut icon in the top right to access a settings panel.
+
+- `speed`: Control the playback rate of samples. A value of `2` will play at double speed. `0.5` will play at half speed. Expects a number > 0.
+  - Note: This is useful for preserving device memory. Set `speed=2` and pitch-shift the sample down by an octave on the K.O. to fit twice as much data.
+- `maxMs`: Cuts off samples after a specified maximum duration, in milliseconds. Useful for kick and crash samples that just. never. end.
+- `gapMs`: If your K.O. is having trouble auto-slicing each drum sample, increasing the milliseconds between sample playback might help. Defaults to no gap, since I tend to trim it manually anyway.
+- `padNames`: I try to keep a specific layout with my kits, but you probably have a different style! You can tweak the text on each pad from 1–16, separated by commas.
+
+#### Default settings
+
+If you want to start the sampler with different settings, bookmark the link generated on the settings pane. For example, the following URL:
+
+[`https://po-33.glitch.me/?speed=2&maxMs=2000&gapMs=20&padNames=1%2C2%2C3%2C4%2C5%2C6%2C7%2C8%2C9%2C10%2C11%2C12%2C13%2C14%2C15%2C16`](https://po-33.glitch.me/?speed=2&maxMs=2000&gapMs=20&padNames=1%2C2%2C3%2C4%2C5%2C6%2C7%2C8%2C9%2C10%2C11%2C12%2C13%2C14%2C15%2C16)
+
+…will start you off at 2x speed, with a max duration of 2s, a gap of 20s, and pads renamed to their numbers.
+
+### File types
+
+This works with whatever file types your browser supports. It should work well with `.wav`, `.mp3`, `.ogg`, etc.
+
+## To do (prioritized)
+
+- ~~Reorder samples~~ done
+- ~~Delete sample~~ done
+- ~~Keyboard control~~ done
+- ~~Show total sample time~~ done
+- ~~Configurable maximum sample length limit~~ done
+- ~~Configurable gap between samples~~ done
+- ~~Configurable speed / pitch control (eg. save memory by playing samples at 2x speed and slowing them down on the device)~~ done
+- ~~Remove the URL search params, add a settings menu~~ done
+- Default sounds (808?)
+- Upload files on click
+- Other tools than just drums?
+
+## See also
+
+[PO-33 Scale App](https://punkyv4n.me/po-33-scale-app/)
+
+### License
+
+[GNU GPLv3](https://glitch.com/edit/#!/po-33?path=LICENSE.md%3A1%3A0)
---
+++ script.js
@@ -0,0 +1,311 @@
+const { Sortable } = window.Draggable;
+
+function updateUI() {
+  updatePadNames();
+  updateTotalSampleLength();
+  updateInputs();
+  updateSettingsLink();
+}
+
+function updatePadNames() {
+  pads.forEach(
+    (pad, i) =>
+      (pad.querySelector("button").textContent = settings.padNames[i] || "*")
+  );
+}
+
+function updateTotalSampleLength() {
+  const length = parseFloat(
+    sources
+      .filter(x => x)
+      .reduce(
+        (a, b, i) =>
+          a +
+          Math.min(settings.maxMs / 1000, b.buffer.duration / settings.speed) +
+          (i ? settings.gapMs / 1000 : 0),
+        0
+      )
+      .toFixed(1)
+  );
+  sampleLengthContainer.textContent = length;
+  secondsContainer.textContent = length === 1 ? "second" : "seconds";
+}
+
+function updateInputs() {
+  ["speed", "maxMs", "gapMs"].forEach(
+    (setting, i) => (inputs[i].value = settings[setting])
+  );
+  inputs[3].value = settings.padNames
+    .map(name => name.replace(/,/g, "\\,"))
+    .join(", ");
+}
+
+function updateSettingsLink() {
+  const { location } = window;
+  document.querySelector(".settings-link").href = `${location.origin}${
+    location.pathname
+  }?speed=${settings.speed}&maxMs=${settings.maxMs}&gapMs=${
+    settings.gapMs
+  }&padNames=${encodeURIComponent(
+    settings.padNames.map(name => name.replace(/,/g, "\\,")).join(",")
+  )}`;
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const settings = {
+  speed: parseFloat(urlParams.get("speed")) || 1,
+  maxMs: parseFloat(urlParams.get("maxMs")) || 2147483647,
+  gapMs: parseFloat(urlParams.get("gapMs")) || 0,
+  padNames: decodeURIComponent(
+    urlParams.get("padNames") || "bd,sn,ho,hc,bd,sn,ho,hc,bd,sn,*,cy"
+  )
+    .split(/(?<!\\),/)
+    .map(name => name.replace(/\\,/g, ","))
+};
+
+const pads = Array.from({ length: 16 }, (_, i) => {
+  const li = document.createElement("li");
+  li.className = "empty";
+  const button = document.createElement("button");
+  li.append(button);
+  return li;
+});
+const sampler = document.querySelector(".sampler-pane");
+const container = document.createElement("ol");
+container.className = "pads";
+container.append(...pads);
+const playButtonWrapper = document.createElement("div");
+playButtonWrapper.id = "play-button-wrapper";
+const playButton = document.createElement("button");
+playButton.className = "play-all";
+const sortable = new Sortable(container, {
+  draggable: ".pads li",
+  distance: 10
+});
+
+const inputs = document.querySelectorAll("input");
+["speed", "maxMs", "gapMs"].forEach((setting, i) => {
+  inputs[i].addEventListener("change", e => {
+    settings[setting] = +e.target.value;
+    updateTotalSampleLength();
+    updateSettingsLink();
+  });
+});
+inputs[3].addEventListener("change", e => {
+  settings.padNames = e.target.value
+    .replace(/ /g, "")
+    .split(/(?<!\\),/)
+    .map(name => name.replace(/\\,/g, ","));
+  updatePadNames();
+  updateSettingsLink();
+});
+
+const infoBox = document.createElement("div");
+infoBox.className = "info-box";
+const sampleLengthContainer = document.createElement("span");
+sampleLengthContainer.innerText = "0";
+const secondsContainer = document.createElement("span");
+secondsContainer.innerText = "seconds";
+infoBox.append(
+  sampleLengthContainer,
+  document.createTextNode(" "),
+  secondsContainer
+);
+
+let draggedOut;
+sortable.on("drag:over:container", e => {
+  draggedOut = false;
+});
+sortable.on("drag:out:container", e => {
+  draggedOut = true;
+});
+sortable.on("sortable:stop", ({ newIndex, oldIndex, dragEvent }) => {
+  if (draggedOut) {
+    dragEvent.data.originalSource.className = "empty";
+    sources[oldIndex] = undefined;
+    updateTotalSampleLength();
+  } else sourceOrder.splice(newIndex, 0, sourceOrder.splice(oldIndex, 1)[0]);
+});
+
+const audioContext = new window.AudioContext();
+const sources = Array.from({ length: 16 });
+const sourceOrder = Array.from({ length: 16 }, (_, i) => i);
+
+const initAudio = (data, i) => {
+  const source = audioContext.createBufferSource();
+  const pad = pads[i];
+  const oldSource = sources[i];
+  if (oldSource) {
+    oldSource.stop(0);
+    oldSource.disconnect(0);
+  }
+
+  audioContext.decodeAudioData(
+    data,
+    buffer => {
+      source.buffer = buffer;
+      pad.className = "playing";
+      sources[i] = source;
+      updateTotalSampleLength();
+      playSource(source).then(() => {
+        pad.className = "loaded";
+        playButtonWrapper.className = pads.every(
+          pad => pad.className === "loaded"
+        )
+          ? "loaded"
+          : "";
+      });
+    },
+    e => {
+      console.error(e);
+      pad.className = "failed";
+      playButtonWrapper.className = "";
+    }
+  );
+};
+
+const playSource = source => {
+  return new Promise((resolve, reject) => {
+    source.playbackRate.value = settings.speed;
+    source.connect(audioContext.destination);
+    let ended = false;
+    const end = () => {
+      if (!ended) {
+        ended = true;
+        source.stop(0);
+        source.disconnect(0);
+        resolve();
+      }
+    };
+    source.onended = e => {
+      setTimeout(end, settings.gapMs);
+    };
+    source.start(0);
+    setTimeout(end, settings.maxMs);
+  });
+};
+
+// AudioBufferSourceNodes can only play once: https://developer.mozilla.org/en-US/docs/Web/API/AudioBufferSourceNode/start
+const refreshSource = i => {
+  const source = sources[i];
+  try {
+    source.stop(0);
+  } finally {
+    const newSource = audioContext.createBufferSource();
+    newSource.buffer = source.buffer;
+    return (sources[i] = newSource);
+  }
+};
+
+const play = i => {
+  if (sources[i]) {
+    pads[i].className = "playing";
+    const source = refreshSource(i);
+    return playSource(source).then(() => (pads[i].className = "loaded"));
+  }
+};
+
+const dropEvent = i => e => {
+  e.stopPropagation();
+  e.preventDefault();
+
+  const droppedFiles = e.dataTransfer.files;
+  const numFiles = droppedFiles.length;
+
+  (function processFile(n) {
+    const reader = new FileReader();
+    reader.onload = e => {
+      initAudio(e.target.result, i + n);
+      if (++n < numFiles && i + n < sources.length) processFile(n);
+    };
+    reader.readAsArrayBuffer(droppedFiles[n]);
+  })(0);
+};
+
+const blockEvent = e => {
+  e.stopPropagation();
+  e.preventDefault();
+  return false;
+};
+
+pads.forEach((pad, i) => {
+  pad.addEventListener("drop", dropEvent(i), false);
+  pad.addEventListener("dragover", blockEvent, false);
+  pad.querySelector("button").addEventListener("click", () => play(i), false);
+});
+document.body.addEventListener("drop", blockEvent, false);
+document.body.addEventListener("dragover", blockEvent, false);
+
+const playAll = e => {
+  const { className } = playButtonWrapper;
+  playButtonWrapper.className = "playing";
+  const playNext = i => {
+    const playing = play(sourceOrder[i]);
+    if (i >= sources.length) return (playButtonWrapper.className = className);
+    else if (playing) {
+      playing.then(() => playNext(i + 1));
+    } else {
+      playNext(i + 1);
+    }
+  };
+  playNext(0);
+};
+
+updateUI();
+playButton.append(document.createTextNode("Play all"));
+playButton.addEventListener("click", playAll, false);
+playButtonWrapper.append(playButton, infoBox);
+sampler.append(container, playButtonWrapper);
+
+let openPane = "sampler";
+const toggles = Array.from(document.querySelectorAll(".toggle"));
+const panes = Array.from(document.querySelectorAll(".pane"));
+const namedPanes = panes.reduce((acc, pane) => {
+  acc[pane.className.match(/([^- ]+)-pane/)[1]] = pane;
+  return acc;
+}, {});
+toggles.forEach(toggle => {
+  const targetPane = toggle.className.match(/([^- ]+)-toggle/)[1];
+  toggle.addEventListener("click", () => {
+    if (openPane === targetPane) {
+      openPane = "sampler";
+      toggle.classList.remove("open");
+      namedPanes[targetPane].classList.remove("open-pane");
+      namedPanes.sampler.classList.add("open-pane");
+    } else {
+      toggles.forEach(t => t.classList.remove("open"));
+      panes.forEach(p => p.classList.remove("open-pane"));
+      toggle.classList.add("open");
+      namedPanes[targetPane].classList.add("open-pane");
+      openPane = targetPane;
+    }
+  });
+});
+
+const keyMap = [
+  "Digit1",
+  "Digit2",
+  "Digit3",
+  "Digit4",
+  "KeyQ",
+  "KeyW",
+  "KeyE",
+  "KeyR",
+  "KeyA",
+  "KeyS",
+  "KeyD",
+  "KeyF",
+  "KeyZ",
+  "KeyX",
+  "KeyC",
+  "KeyV"
+];
+document.addEventListener(
+  "keydown",
+  e => {
+    if (e.repeat || openPane !== "sampler") return;
+    const i = keyMap.indexOf(e.code);
+    if (i >= 0) play(sourceOrder[i]);
+  },
+  false
+);
